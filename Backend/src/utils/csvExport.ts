0da1e13fb552d91c0c14d0f3fdae9d6a09986dbc
Asheem Rahman <mhdasheem@gmail.com>
2025-12-05 23:20:30 +0530

    Fix : csv file download

--- Backend/src/utils/csvExport.ts
+++ Backend/src/utils/csvExport.ts
@@ -2,7 +2,10 @@ export const exportCSV = async (expenses: any[]) => {
     const header = "title,amount,category,date\n";
 
     const rows = expenses
-        .map((e) => `${e.title},${e.amount},${e.category.name},${e.date.toISOString()}`)
+        .map((e) => {
+            const date = new Date(e.date).toISOString().split("T")[0]; // YYYY-MM-DD
+            return `${e.title},${e.amount},${e.category_name},${date}`;
+        })
         .join("\n");
 
     return header + rows;
